aae5123784cf2ebbbc67213e7cea2251254437ad
imnotpizza <boooong772@naver.com>
2021-06-23 00:24:37 +0900

    hoc 공부중

--- src/containers/SampleContainer.js
+++ src/containers/SampleContainer.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import { connect } from 'react-redux';
 import Sample from '../components/Sample';
 import { getPost, getUsers } from '../modules/sample';
-
+//fefefefef
 const SampleContainer = ({
     getPost,
     getUsers,
